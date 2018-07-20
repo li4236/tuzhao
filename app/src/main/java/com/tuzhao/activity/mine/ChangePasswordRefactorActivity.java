54cf87955dc4be1a6fd0487e7939832d84c06cce
zhongjuncoder <1247660633@qq.com>
2018-07-20 15:07:06 +0800

    添加修改密码

---
+++ app/src/main/java/com/tuzhao/activity/mine/ChangePasswordRefactorActivity.java
@@ -0,0 +1,62 @@
+package com.tuzhao.activity.mine;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tuzhao.R;
+import com.tuzhao.activity.base.BaseStatusActivity;
+import com.tuzhao.publicmanager.UserManager;
+
+/**
+ * Created by juncoder on 2018/7/20.
+ */
+public class ChangePasswordRefactorActivity extends BaseStatusActivity {
+
+    private TextView mChangePassword;
+
+    private TextView mTelephone;
+
+    @Override
+    protected int resourceId() {
+        return R.layout.activity_change_password_layout;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        mChangePassword = findViewById(R.id.change_passwrod_hint);
+        mTelephone = findViewById(R.id.telephone_number_tv);
+
+        findViewById(R.id.origin_password_cv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.telephone_number_cv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        mChangePassword.setText("为保障用户账号安全\n修改密码请确认身份");
+        StringBuilder telephone = new StringBuilder(UserManager.getInstance().getUserInfo().getUsername());
+        if (telephone.length() > 6) {
+            telephone.replace(3, 8, "*****");
+        }
+        mTelephone.setText(telephone.toString());
+    }
+
+    @NonNull
+    @Override
+    protected String title() {
+        return "修改密码";
+    }
+
+}
